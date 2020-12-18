ace866fb4291eadb929838dae8daf8d051fbaece
Evan Ochs <ochsevan@gmail.com>
2020-12-18 11:01:15 -0500

    scroll animation

---
+++ scroll-animation/script.js
@@ -0,0 +1,20 @@
+const boxes = document.querySelectorAll(".box");
+
+window.addEventListener("scroll", checkBoxes);
+
+//Without this the intial boxes wont load at the launch of the page
+checkBoxes();
+
+function checkBoxes() {
+  const triggerBottom = (window.innerHeight / 5) * 4;
+
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+
+    if (boxTop < triggerBottom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
+}
